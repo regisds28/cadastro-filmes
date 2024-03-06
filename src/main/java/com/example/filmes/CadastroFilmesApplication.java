e6d19ee60ca88f68f6c7684e8503bcd9bd2dc799
Régis Paiva Araújo <regisparaujo@gmail.com>
2024-03-06 15:06:47 -0300

    criando cadastro de filmes e séries

---
+++ src/main/java/com/example/filmes/CadastroFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFilmesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroFilmesApplication.class, args);
+    }
+
+}
